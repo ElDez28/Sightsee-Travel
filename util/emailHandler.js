bc4c4b662921c64775a303203d337268251ac83e
ElDez28 <dezmic91@gmail.com>
2023-02-12 21:00:56 +0100

    Email bug solved

--- util/emailHandler.js
+++ util/emailHandler.js
@@ -28,7 +28,7 @@ const sendEmail = async (options) => {
         </div>
         <p style="font-size:1.1em">Hi,</p>
         <p>Thank you for choosing Seesight travel. Click on the following link to reset your password</p>
-        <a style="background:#50C878; padding: 0.5rem 4rem; margin: 0 auto; text-decoration:none; color:white" href=http://localhost:3000/users/resetPassword/${options.resetToken}>Reset password</a>
+        <a style="background:#50C878; padding: 0.5rem 4rem; margin: 0 auto; text-decoration:none; color:white" href=https://seesight-next.vercel.app/users/resetPassword/${options.resetToken}>Reset password</a>
         <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;"></h2>
         <p style="font-size:0.9em;">Regards,<br />Seesight travel</p>
         <hr style="border:none;border-top:1px solid #eee" />
